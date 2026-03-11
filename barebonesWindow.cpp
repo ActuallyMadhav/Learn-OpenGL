8233458e1beb0eacdb59a6b59920249aca305371
ActuallyMadhav <madhavra.2004@gmail.com>
2026-03-11 13:31:40 +0800

    commented out so there is only 1 main

--- barebonesWindow.cpp
+++ barebonesWindow.cpp
@@ -2,32 +2,32 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
-int main(){
+// int main(){
 
-    // glfw initialization
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+//     // glfw initialization
+//     glfwInit();
+//     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+//     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+//     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+//     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-    // window creation
-    GLFWwindow* window = glfwCreateWindow(800, 600, "bare-bones test", NULL, NULL);
-    glfwMakeContextCurrent(window);
+//     // window creation
+//     GLFWwindow* window = glfwCreateWindow(800, 600, "bare-bones test", NULL, NULL);
+//     glfwMakeContextCurrent(window);
 
-    // GLAD initialzation
-    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
-        std::cout << "Failed to init GLAD" << '\n';
-        return -1;
-    }
+//     // GLAD initialzation
+//     if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+//         std::cout << "Failed to init GLAD" << '\n';
+//         return -1;
+//     }
 
-    // rendering loop
-    while(!glfwWindowShouldClose(window)){
-        glClear(GL_COLOR_BUFFER_BIT);
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
+//     // rendering loop
+//     while(!glfwWindowShouldClose(window)){
+//         glClear(GL_COLOR_BUFFER_BIT);
+//         glfwSwapBuffers(window);
+//         glfwPollEvents();
+//     }
 
-    glfwTerminate();
-    return 0;
-}
+//     glfwTerminate();
+//     return 0;
+// }
