81126d79117336bae94d5f7aec48fb5121a42a37
ActuallyMadhav <madhavra.2004@gmail.com>
2026-03-25 13:53:39 +0800

    edited include path to add glm. glm test as well

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
 #include <iostream>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
@@ -42,6 +43,9 @@ int main(){
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
+    glm::vec2 testVec = {1.0f, 2.0f};
+    std::cout << testVec.x << ' ' << testVec.y << '\n';
     
     glfwTerminate();
     return 0;
