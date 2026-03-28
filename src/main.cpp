ec08fc4beac550397d5a0f4e1750a5ed045d5e78
ActuallyMadhav <madhavra.2004@gmail.com>
2026-03-28 12:30:51 +0800

    cleaned file abit

--- src/main.cpp
+++ src/main.cpp
@@ -14,20 +14,6 @@ std::string loadShaderSource(const char* path);
 const unsigned int width = 600;
 const unsigned int height = 800;
 
-// const char* vertexShaderSource = "#version 330 core\n"
-//     "layout (location = 0) in vec3 aPos;\n"
-//     "void main()"
-//     "{\n"
-//     "   gl_Position = vec4(aPos.x, aPos. y, aPos.z, 1.0);\n"
-//     "}\0";
-
-// const char* fragmentShaderSource = "#version 330 core\n"
-//     "out vec4 FragColor;\n"
-//     "void main()"
-//     "{\n"
-//     "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-//     "}\n";
-
 std::string vertCode = loadShaderSource("shaders/shader.vert");
 std::string fragCode = loadShaderSource("Shaders/shader.frag");
 
